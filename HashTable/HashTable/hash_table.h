dc26dcf7eba4aa25ca34a65e5d38805fb8827eec
Bucket <janlucaloettgen@gmail.com>
2021-04-22 14:51:53 +0200

    v:1.01 updated header file according to updates to hash table functions.

--- HashTable/HashTable/hash_table.h
+++ HashTable/HashTable/hash_table.h
@@ -6,15 +6,19 @@
 * which stores the linked lists, and also stores the tables size.
 * 
 */
+typedef long long (*hash_func)(char* key);
+
 struct hash_table {
 	struct node** root_arr;
 	int table_size;
+	hash_func hash_func;
 };
 
 typedef struct hash_table hash_table;
-
 //external methods 
-hash_table* hash_table_init(int table_size);
+long long hash_func_poly(char* key);
+
+hash_table* hash_table_init(int table_size, hash_func hash_func);
 
 void hash_add_item(hash_table** hash_table, char* key, const void* ptr);
 
@@ -22,8 +26,10 @@ void hash_print_table(hash_table** hash_table);
 
 const void* hash_find_item(hash_table** hash_table, char* key);
 
-const void* hash_deallocate(hash_table** hash_table);
+void hash_deallocate(hash_table** hash_table);
 
 void hash_remove_item(hash_table** hash_table, char* key);
+
+void hash_rehash(hash_table** hash_table, int new_table_size, hash_func hash_func);
 
 #endif
